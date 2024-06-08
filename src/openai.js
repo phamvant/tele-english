292c5000c7d8b6d726eb917b695304b784b528f4
thuan <pham.t.286.01@gmail.com>
2024-06-08 20:50:39 +0900

    init project

---
+++ src/openai.js
@@ -0,0 +1,31 @@
+// import Groq from "groq-sdk";
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: process.env.AI_APIKEY,
+});
+
+export async function callAI(prop) {
+  const chatCompletion = await getGroqChatCompletion(prop);
+  // Print the completion returned by the LLM.
+  return chatCompletion.choices[0]?.message?.content || "";
+}
+
+async function getGroqChatCompletion(prop) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You're an English teacher. Just check the sentences and provide the right one",
+      },
+      {
+        role: "user",
+        content: prop,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+// module.exports = { callAI };
